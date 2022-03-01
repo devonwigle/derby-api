b9878ab0e78f874a82df30e2a3ca89fb16c265dc
Devon Wigle <devonwigle@gmail.com>
2022-03-01 15:11:53 -0600

    Attempt cors work around

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ let cors = require('cors');
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser')
 
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'Roller Derby Rules';
 app.locals.whistles = [
